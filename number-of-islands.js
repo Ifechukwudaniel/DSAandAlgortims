19c24a9be2aa8d5c023841fcd443ed81b8307952
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-25 15:41:21 +0100

    If you find an island , increase the island count and  replace everything  land with water
    https://leetcode.com/problems/number-of-islands/

--- number-of-islands.js
+++ number-of-islands.js
@@ -1,20 +1,35 @@
-var numIslands = function (grid) {
-  let directions = [
-    [-1, 0], // up
-    [0, 1], // right
-    [1, 0], // down
-    [0, -1], //left
-  ];
+let directions = [
+  [-1, 0], // up
+  [0, 1], // right
+  [1, 0], // down
+  [0, -1], //left
+];
 
-  let seen = Array(grid.length)
-    .fill(0)
-    .map(() => Array(grid[0].length).fill(false));
+function dfs(row, col, grid) {
+  if (row < 0 || col < 0) return;
+  if (row >= grid.length || col >= grid[0].length) return;
+  let
 
-  for (let i = 0; i < directions.length; i++) {
-    const element = array[i];
+  if (grid[row][col] === "1") {
+    grid[row][col] = "0";
+    for (let i = 0; i < directions.length; i++) {
+      let dir = directions[i];
+      dfs(row + dir[0], col + dir[1], grid);
+    }
   }
+}
 
-  console.log(seen);
+var numIslands = function (grid) {
+  let count = 0;
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      if (grid[row][col] === "1") {
+        count++;
+        dfs(row, col, grid);
+      }
+    }
+  }
+  return count;
 };
 
 let grid = [
