9c8aeb124669247c7dcd94a7f3c6742503def25b
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-27 16:24:09 +0100

    Binary Tree  Preorder traversal
    The recurse  current node comes first , the left node  and then the right node
    
    https://leetcode.com/problems/binary-tree-preorder-traversal/

---
+++ BinaryTree/BinaryTreePreOrder.js
@@ -0,0 +1,20 @@
+const { insertLevelOrder } = require("./BinaryTreeFromArray");
+
+var preorderTraversal = function (root) {
+  if (root == null) return [];
+
+  let array = [];
+
+  function preorder(node) {
+    if (node == null) return;
+    array.push(node.val);
+    preorder(node.left);
+    preorder(node.right);
+    return array;
+  }
+
+  return preorder(root);
+};
+
+let root = insertLevelOrder([1, null, 2, 3]);
+console.log(preorderTraversal(root));
