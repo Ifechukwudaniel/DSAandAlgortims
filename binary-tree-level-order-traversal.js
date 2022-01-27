b7e2c4afe6c097a482d9ed72872e798b131da8dc
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-27 13:06:15 +0100

    refactored for binary tree

--- binary-tree-level-order-traversal.js
+++ binary-tree-level-order-traversal.js
@@ -1,20 +1,4 @@
-function TreeNode(val) {
-  this.left = null;
-  this.right = null;
-  this.val = val;
-}
-
-// Function to insert nodes in level order
-function insertLevelOrder(arr, root, i) {
-  // Base case for recursion
-  if (i < arr.length && arr[i] !== null) {
-    let temp = new TreeNode(arr[i]);
-    root = temp;
-    root.left = insertLevelOrder(arr, root.left, 2 * i + 1);
-    root.right = insertLevelOrder(arr, root.right, 2 * i + 2);
-  }
-  return root;
-}
+const { insertLevelOrder } = require("./BinaryTree/BinaryTreeFromArray");
 
 var levelOrder = function (root) {
   if (!root) return [];
--- invert-binary-tree.js
+++ invert-binary-tree.js
@@ -1,24 +1,10 @@
-function TreeNode(val) {
-  this.left = null;
-  this.right = null;
-  this.val = val;
-}
-
-// Function to insert nodes in level order
-function insertLevelOrder(arr, root, i) {
-  // Base case for recursion
-  if (i < arr.length && arr[i] !== null) {
-    let temp = new TreeNode(arr[i]);
-    root = temp;
-    root.left = insertLevelOrder(arr, root.left, 2 * i + 1);
-    root.right = insertLevelOrder(arr, root.right, 2 * i + 2);
-  }
-  return root;
-}
+const {
+  insertLevelOrder,
+  toArray,
+} = require("./BinaryTree/BinaryTreeFromArray");
 
 var invertTree = function (root) {
   if (root == null) return null;
-
   let left = root.left,
     right = root.right;
   root.right = invertTree(left);
@@ -28,11 +14,8 @@ var invertTree = function (root) {
 
 let arr = [4, 2, 7, 1, 3, 6, 9];
 let root = insertLevelOrder(arr, null, 0);
-console.log(root);
-invertTree(root);
-console.log(root);
+console.log(toArray(invertTree(root)));
 
 arr = [2, 1, 3];
 root = insertLevelOrder(arr, null, 0);
-console.log(root);
-invertTree(root);
+console.log(toArray(invertTree(root)));
