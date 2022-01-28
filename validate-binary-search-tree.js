5f696efaa9df31c80779f863d2cef0df60ffea27
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-28 19:01:25 +0100

    Validate a bst using dfs, the max value at the first iteration  is  Infinity and the min is -Infinity,
    comaple if value is grater than max an less than min
    
    https://leetcode.com/problems/validate-binary-search-tree/

---
+++ validate-binary-search-tree.js
@@ -0,0 +1,28 @@
+const { insertLevelOrder } = require("./BinaryTree/BinaryTreeFromArray");
+
+var dfs = (node, min, max) => {
+  if (!node) return node;
+  if (node.val <= min || node.val >= max) return false;
+
+  if (node.left) {
+    if (!dfs(node.left, min, node.val)) return false;
+  }
+
+  if (node.right) {
+    if (!dfs(node.right, node.val, max)) return false;
+  }
+
+  return true;
+};
+
+var isValidBST = function (root) {
+  return dfs(root, -Infinity, Infinity);
+};
+
+let root = [2, 1, 3];
+root = insertLevelOrder(root);
+console.log(isValidBST(root));
+
+root = [5, 1, 4, null, null, 3, 6];
+root = insertLevelOrder(root);
+console.log(isValidBST(root));
