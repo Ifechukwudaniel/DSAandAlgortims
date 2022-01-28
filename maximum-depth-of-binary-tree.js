20b5c99368c7e7ec69ed702ad5cfa1a001daeb53
Angry Coding <mailidanielifechukwu@gmail.com>
2022-01-28 08:53:12 +0100

    max depth changes

--- maximum-depth-of-binary-tree.js
+++ maximum-depth-of-binary-tree.js
@@ -1,30 +1,12 @@
-function TreeNode(val) {
-  this.left = null;
-  this.right = null;
-  this.val = val;
-}
+const { insertLevelOrder } = require("./BinaryTree/BinaryTreeFromArray");
 
-// Function to insert nodes in level order
-function insertLevelOrder(arr, root, i) {
-  // Base case for recursion
-  if (i < arr.length && arr[i] !== null) {
-    let temp = new TreeNode(arr[i]);
-    root = temp;
-    root.left = insertLevelOrder(arr, root.left, 2 * i + 1);
-    root.right = insertLevelOrder(arr, root.right, 2 * i + 2);
-  }
-  return root;
-}
-
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-root = insertLevelOrder(arr, null, 0);
-
-// Using Dfs count the items 
-// And compare the right and left for the greater depth
 var maxDepth = function (root, count = 0) {
   if (root === null) return count;
   count++;
   return Math.max(maxDepth(root.left, count), maxDepth(root.right, count));
 };
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+root = insertLevelOrder(arr);
 
 console.log(maxDepth(root));
