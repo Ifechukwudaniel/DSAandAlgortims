3e26b9289f29ea1c1044568d320acc9a5ae46d2f
Angry Coding <mailidanielifechukwu@gmail.com>
2022-02-15 17:38:39 +0100

    changes

--- search-a-2d-matrix-ii.js
+++ search-a-2d-matrix-ii.js
@@ -1,8 +1,12 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
 var searchMatrix = function (matrix, target) {
   let i = 0;
   let j = matrix[0].length - 1;
-  while (i <= matrix.length - 1 && j >= 0) {
-    console.log(i, j);
+  while (i < matrix.length && j >= 0) {
     if (matrix[i][j] > target) {
       j--;
       continue;
@@ -14,9 +18,17 @@ var searchMatrix = function (matrix, target) {
       i++;
       continue;
     }
-    return false;
   }
+  return false;
 };
+
+let matrix = [
+  [1, 3, 5, 7],
+  [10, 11, 16, 20],
+  [23, 30, 34, 60],
+];
+let target = 60;
+console.log(searchMatrix(matrix, target));
 
 //[
 //. [1,4,7,11,15],
