96229e75f6fabbc41217a9ec2b5b56f94592913a
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-02-19 01:56:49 +0100

    Time: 76 ms (94.76%), Space: 44.5 MB (16.75%) - LeetHub

---
+++ 402-remove-k-digits/402-remove-k-digits.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} num
+ * @param {number} k
+ * @return {string}
+ */
+var removeKdigits = function(num, k) {
+    let stack = []
+    for(let i = 0; i< num.length; i++){
+        while(k>0 && stack[stack.length-1] > num[i]){
+             stack.pop()
+             k--
+        }
+        stack.push(num[i])
+    } 
+    if(k>0){
+        stack = stack.slice(0,stack.length - k)
+    }
+    
+    if(stack.length === 0) return "0"
+    
+    return String(BigInt(stack.join("")))
+};
