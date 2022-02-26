269535b6dfdd007a3545a2a786e7649414f123d9
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-02-26 13:57:21 +0100

    Time: 161 ms (73.59%), Space: 52.2 MB (52.66%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -2,18 +2,24 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+
+ const createHash = (string) => {
+     let  array= new Array(26).fill(0)
+     for(const letter of string){
+        array[letter.charCodeAt(0) - 97]+= 1
+     }
+     return array.join('#')
+}
 var groupAnagrams = function(strs) {
-    let hashMap = new Map()
-    
+    let hashMap = new Map() 
     for(let i =0 ; i<strs.length; i++){
-        let hash = strs[i].split('').sort((a,b)=>a.localeCompare(b)).join("")
+        let hash = createHash(strs[i])
         if(!hashMap.has(hash)){
            hashMap.set(hash,[strs[i]])
         }
         else {
             let prev= hashMap.get(hash)
             prev.push(strs[i])
-            // hashMap.set(hash,prev)
         }
     }
     
