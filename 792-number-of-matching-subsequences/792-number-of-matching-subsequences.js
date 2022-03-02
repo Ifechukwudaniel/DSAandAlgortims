103683205b3bdfb5710dc0aa3ac9ea36c1545a3e
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-02 12:09:47 +0100

    Time: 268 ms (47.95%), Space: 57.4 MB (25.00%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
@@ -1,46 +1,44 @@
-/**
- * @param {string} s
- * @param {string[]} words
- * @return {number}
- */ 
-
-
-var numMatchingSubseq = function(s, words) {
-  let hashMap =  new Map()
-   for (let i =0 ; i<s.length ; i++){
-      if(!hashMap.has(s[i])){
-          hashMap.set(s[i],[i])
-      } 
-      else hashMap.get(s[i]).push(i)
-   }
-
-   let count = 0
-   
-   for (let i=0; i<words.length; i++){
-      let prev = -1
-      let matchFlag = true;
-      for(let char of words[i] ){
-        if(!hashMap.has(char)){
-            matchFlag = false
-            break
+var numMatchingSubseq = function(S, words) {
+    let count = 0, arrPos = [];
+    
+    // create the position Array of chars in S
+    for(let i = 0; i < S.length; i++){
+        let charCode = S[i].charCodeAt(0);
+        if(arrPos[charCode]) arrPos[charCode].push(i)
+        else arrPos[charCode] = [i];
+    }
+    
+    // special binarySearch that return index if found, otherwise negative position
+    const binarySearch = (arr, val) => {
+        let i = 0, j = arr.length - 1, mod;
+        while(i <= j){
+            mod = Math.floor((i + j) / 2);
+            if(val < arr[mod])  j = mod - 1;
+            else if(val > arr[mod]) i = mod + 1;
+            else return mod;
         }
-        else {
-          let charPos = hashMap.get(char)
-          let r= charPos.length -1
-          let l = 0
-          while(l<r){
-             let mid = Math.floor((r + l) / 2);
-             if(charPos[mid] < prev) l = mid+1 
-             else  r  = mid
-           }
-          if(charPos[l] <  prev) {
-              matchFlag = false;
-              break
-          }
-          prev = charPos[l]+1
-         }
-     }
-     if(matchFlag) count++
-   }
-   return count
-};
+        return -(i + 1);
+    }
+    
+    const isSubSequence = (word, prev) => {
+         // loop through chars of words and check their position using Binary Search
+        for(let j = 0; j < word.length; j++){
+            let charCode = word[j].charCodeAt(0);
+            if(!arrPos[charCode]) return false;
+            let idx = binarySearch(arrPos[charCode], prev);
+            if(idx < 0) idx = -idx - 1;
+            if(idx === arrPos[charCode].length) return false;
+            prev = arrPos[charCode][idx] + 1;
+        }
+        return true;
+    }
+    
+    // loop through words
+    for(let i = 0; i < words.length;i++){
+        let word = words[i];
+        if(isSubSequence(word, 0)) count++;
+    }
+    
+    return count;
+    
+}; 
