4643e904fff3c6196b80093b083a0b9c93503f9a
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-02 10:06:49 +0100

    Time: 6160 ms (10.25%), Space: 51.5 MB (55.74%) - LeetHub

---
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.js
@@ -0,0 +1,34 @@
+/**
+ * @param {string} s
+ * @param {string[]} words
+ * @return {number}
+ */ 
+
+
+var numMatchingSubseq = function(s, words) {
+   let count = 0
+   let cache= {}
+   
+   var isSubSequence= function(s, word){
+       if(cache[word]) return cache[word]
+       
+        let i =0
+        let j =0
+        while(i<word.length&& j<s.length){
+           if(word[i]=== s[j]){
+               i++ 
+               j++
+           } 
+           else  j++
+        }
+        cache[word] = i === word.length
+        return  cache[word]
+    }
+   
+   for(let i= 0; i<words.length; i++){
+       if(isSubSequence(s, words[i])){
+          count++
+       }
+   }
+   return count
+};
