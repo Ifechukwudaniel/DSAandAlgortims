5d0fcc0056b860159c51eed266da0cb2b74c62b8
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-03 10:18:31 +0100

    Time: 91 ms (96.91%), Space: 48.6 MB (28.87%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.js
@@ -5,16 +5,29 @@
  * @return {boolean[]}
  */
 var checkArithmeticSubarrays = function(nums, l, r) {
-    return l.map((firstRange, index) => {
-        let newArray = nums.slice(firstRange, r[index] + 1).sort((a, b) => a - b)
-        let req = newArray[1] - newArray[0]
-        let prevVal = newArray[1]
-        for(let i = 2; i < newArray.length; i++){
-            if(newArray[i] - prevVal !== req){
-                return false
-            }
-            prevVal = newArray[i]
-        }
-        return true
-    })
+	let result = []
+	for (let i = 0; i < l.length; i++) {
+		let min = nums[l[i]]
+		let max = nums[l[i]]
+		let count = 0
+		let sum = 0
+		let set = []
+		for (let j = l[i]; j <= r[i]; j++) {
+			count++;
+			sum += nums[j]
+			min = min > nums[j] ? nums[j] : min;
+			max = max > nums[j] ? max: nums[j];
+			set.push(nums[j])
+		}
+		let d = (max - min)/(count - 1);
+		let possible = true;
+		for (let z = 1; z < set.length; z++) {
+			if (!set.includes(min + z*d)) {
+				possible = false;
+				break;
+			}
+		}
+		result.push(possible)
+	}
+	return result;
 };
