16743a44156dac4cdd3762bce8b01adf7db0387f
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-04 15:50:22 +0100

    Time: 76 ms (67.30%), Space: 42.2 MB (22.81%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.js
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -3,14 +3,23 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
-  const res = [];
-
-    for (let i = 0; i < numRows; i++) {
-        const row = new Array(i + 1).fill(1);
-        for (let j = 1; j < row.length - 1; j++) {
-            row[j] = res[i - 1][j - 1] + res[i - 1][j];
-        }
-        res.push(row);
-    }
-    return res;
+  if(numRows== 1) return [[1]]
+  if(numRows== 2) return [[1],[1,1]]
+    
+  let cache= {}
+  let res =  [[1],[1,1]]
+ 
+  for(let i=2 ; i<numRows; i++){
+      let row= []
+      for( let j = 0 ; j<=i; j++){
+          if(j==0 ||  i-j ==0){
+              row.push(1)
+              continue
+          }else {
+              row.push(res[i-1][j-1] +res[i-1][j])
+          }
+      }
+    res.push(row)
+  }
+  return  res
 };
