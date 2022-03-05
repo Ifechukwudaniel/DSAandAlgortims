7179d3e6159bf2be37c9b53432e7d4e3f99ff634
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-05 11:36:36 +0100

    Time: 71 ms (82.66%), Space: 42.1 MB (28.35%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.js
+++ 509-fibonacci-number/509-fibonacci-number.js
@@ -2,8 +2,15 @@
  * @param {number} n
  * @return {number}
  */
+
 var fib = function(n) {
-    if(n==0) return 0
-    if(n <= 2) return 1
-    return fib(n-1) + fib(n-2)
+    let cache = {}
+    var _fib = function(target){
+       if(target==0) return 0
+       if(target <= 2) return 1
+       if(cache[target]) return cache[target]
+       cache[target] = _fib(target-1) + _fib(target-2)
+       return  cache[target]
+    }
+    return _fib(n)
 };
