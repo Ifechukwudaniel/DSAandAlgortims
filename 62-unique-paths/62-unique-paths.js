3cf4d5ace29da4b0621a8a52a4e5196c7a664d88
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-07 14:56:16 +0100

    Time: 105 ms (30.82%), Space: 42.9 MB (23.19%) - LeetHub

--- 62-unique-paths/62-unique-paths.js
+++ 62-unique-paths/62-unique-paths.js
@@ -4,13 +4,15 @@
  * @return {number}
  */
 var uniquePaths = function(m, n, memo={}) {
-    let key = m+','+n
-    let reverseKey = n+','+m
-    // check memo for current value 
-    if(key in memo) return memo[key]
-    if(reverseKey in memo) return memo[reverseKey]
-    if(m===1 && n === 1) return 1;
-    if(m===0 || n === 0 ) return 0;
-    memo[key] =  uniquePaths(m-1,n, memo) +  uniquePaths(m,n-1,memo);
-    return memo[key]
+ let grid = new Array(m + 1).fill(0).map(() => new Array(n + 1).fill(0));
+  grid[1][1] = 1;
+
+  for (let row = 0; row < grid.length; row++) {
+    for (let col = 0; col < grid[0].length; col++) {
+      let current = grid[row][col];
+      if (row + 1 <= m) grid[row + 1][col] += current;
+      if (col + 1 <= n) grid[row][col + 1] += current;
+    }
+  }
+  return grid[m][n];
 };
