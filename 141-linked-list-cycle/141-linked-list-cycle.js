eda12c689db872c2243fe93363a623f799ff39ae
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-08 15:16:05 +0100

    Time: 87 ms (74.16%), Space: 45.5 MB (13.24%) - LeetHub

---
+++ 141-linked-list-cycle/141-linked-list-cycle.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+var hasCycle = function(head) {
+    if(!head) return false
+    if(!head.next) return false
+    
+    let fast = head ,slow = head
+    
+    while(fast !== null ){
+       if(!fast.next)  return  false
+        slow = slow.next 
+        fast = fast.next.next
+        if(slow === fast) return true
+    }
+    return false
+};
