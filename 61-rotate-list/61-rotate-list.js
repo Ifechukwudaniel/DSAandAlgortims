0c992b5d31d8d4eec0904eea8ccf2f894ecf9e1b
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-11 12:07:05 +0100

    Time: 184 ms (8.88%), Space: 45.7 MB (5.54%) - LeetHub

--- 61-rotate-list/61-rotate-list.js
+++ 61-rotate-list/61-rotate-list.js
@@ -10,31 +10,29 @@
  * @param {number} k
  * @return {ListNode}
  */
-
- var rotateRight = function (head, k = 1) {
-  if (k == 0) return head;
-  if (!head) return head;
-  if (!head.next) return head;
-  let length = 1;
-  let lastNode = null;
-  let tail = head;
-
-  while (tail.next) {
-    tail = tail.next;
-    length++;
-  }
-
-  if (k >= length) k = k % length;
-
-  if (k == 0) return head;
-
-  let currentNode = head;
-  for (let i = 0; i < length - k - 1; i++) {
-    currentNode = currentNode.next;
-  }
-
-  lastNode = currentNode.next;
-  currentNode.next = null;
-  tail.next = head;
-  return lastNode;
+var rotateRight = function(head, k) {
+    if(!head) return null
+    let length = 1
+    let temp = head
+    let tail = head;
+    while(tail.next){
+       tail =  tail.next
+       length++
+    }
+    temp = head
+    let prev = temp
+    
+    k =  k%length
+    
+    if(k==0) return head
+    
+    for( let i=0; i<length-k-1; i++){
+        console.log(i)
+        temp= temp.next
+    }
+    
+    let lastNode = temp.next
+    temp.next= null
+    tail.next = head;
+    return  lastNode
 };
