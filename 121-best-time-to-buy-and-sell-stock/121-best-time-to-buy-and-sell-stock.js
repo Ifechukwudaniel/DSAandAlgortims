aa6a3bb843492fd7e347d5f3a099887254c658e1
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-11 09:19:50 +0100

    Time: 110 ms (64.11%), Space: 51.9 MB (36.33%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let min = prices[0]
+    let max = 0
+    
+    for(let i=1 ; i<prices.length; i++){
+        min = Math.min(min,prices[i])
+        max = Math.max(max,prices[i] - min)
+    }
+    return max
+};
