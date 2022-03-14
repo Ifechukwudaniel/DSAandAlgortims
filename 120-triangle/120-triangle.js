7b320b3569cb83c7cd8e7de60acd6614f5117589
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-03-14 18:21:38 +0100

    Time: 95 ms (51.21%), Space: 42.5 MB (53.09%) - LeetHub

--- 120-triangle/120-triangle.js
+++ 120-triangle/120-triangle.js
@@ -6,8 +6,10 @@ var minimumTotal = function(triangle) {
     let cache = triangle[triangle.length - 1]
     
     for(let i = triangle.length -2 ; i>=0; i--){
+        let layer = triangle[i]
+        
         for (let j = 0 ; j< triangle[i].length; j++){
-          cache[j] = triangle[i][j]+Math.min(cache[j], cache[j+1])
+          cache[j] = layer[j]+Math.min(cache[j], cache[j+1])
         }
     }
     
