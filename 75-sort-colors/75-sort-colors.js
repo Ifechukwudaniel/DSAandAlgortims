7e4fe3769afbae60d9230ee0a03c1a63b785c961
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-12 18:02:51 +0100

    Time: 73 ms (65.55%), Space: 41.7 MB (90.56%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var sortColors = function(nums) {
+    
+    let count =[0,0,0];
+    
+    for(let i of nums)
+    {count[i]++}
+    
+    count[1]+= count[0]
+    count[2]+= count[1]
+    
+    for(let i =0; i<count[0]; i++){
+       nums[i]= 0
+    }
+    
+    for(let i =count[0]; i<count[1]; i++){
+      nums[i]= 1
+    }
+    
+    for(let i =count[1]; i<count[2]; i++){
+      nums[i]= 2
+    }
+    
+    return nums
+};
