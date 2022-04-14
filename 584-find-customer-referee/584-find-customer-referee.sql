d4a12dafd5ffa2d4818179dfcced5a1787f64dc3
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-14 11:06:11 +0100

    Time: 508 ms (43.08%), Space: 0B (100.00%) - LeetHub

--- 584-find-customer-referee/584-find-customer-referee.sql
+++ 584-find-customer-referee/584-find-customer-referee.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-Select name  from  Customer where  referee_id  is Null or referee_id  != 2
+SELECT name FROM customer WHERE referee_id <> 2 OR referee_id IS NULL;
