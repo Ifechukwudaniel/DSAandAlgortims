796504932956ea0bd6fc5a09769870a64b716795
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-15 17:00:54 +0100

    Time: 290 ms (27.86%), Space: 0B (100.00%) - LeetHub

--- 627-swap-salary/627-swap-salary.sql
+++ 627-swap-salary/627-swap-salary.sql
@@ -1,7 +1,7 @@
 # Write your MySQL query statement below
 UPDATE salary
-SET
-    sex = CASE sex
-        WHEN 'm' THEN 'f'
-        ELSE 'm'
-    END;
+SET 
+    sex = CASE sex 
+        WHEN 'm' THEN 'f' 
+        ELSE "m" 
+    END; 
