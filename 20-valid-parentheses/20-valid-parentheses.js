cf7d1d8f1a1aa3657b1bcfd026781f954f6628d5
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-15 14:51:11 +0100

    Time: 71 ms (73.96%), Space: 42.5 MB (40.69%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -7,14 +7,19 @@ var isValid = function (string) {
   // our stack 
   let stack = []
   
+  /// pairs 
   let pairs = {
       "[":"]","{":"}","(":")",
   }
   
+  ///loop through
   for (let char of string){
+      // if is pari add the complemt to the stack
      if(pairs[char] != undefined)  stack.push(pairs[char]);
      else {
+          // pop te complement 
         let leftPair =  stack.pop()
+          // return if it does not match  the complement 
         if(leftPair !== char) return false
      }
   }
