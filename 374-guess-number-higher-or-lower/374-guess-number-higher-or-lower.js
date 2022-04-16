92b06bea3dd412d9ba906eb24e6e6ca9ce000199
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-16 20:55:13 +0100

    Time: 59 ms (89.52%), Space: 41.8 MB (67.93%) - LeetHub

---
+++ 374-guess-number-higher-or-lower/374-guess-number-higher-or-lower.js
@@ -0,0 +1,30 @@
+/** 
+ * Forward declaration of guess API.
+ * @param {number} num   your guess
+ * @return 	            -1 if num is lower than the guess number
+ *			             1 if num is higher than the guess number
+ *                       otherwise return 0
+ * var guess = function(num) {}
+ */
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var guessNumber = function(n) {
+    let left = 1
+    let right = n
+    
+    while(left<= right){
+       let mid = Math.floor((left+ (right-left)/2))
+       if(guess(mid) === -1 ) right = mid   
+       if(guess(mid) === 1 ) left =mid+1 
+       if(guess(mid) === 0 ) return mid    
+    }
+    return left
+};
+
+//10 9
+//1+10/2 = 5 
+//6+10/2 = 8
+//8+10/2 = 9
