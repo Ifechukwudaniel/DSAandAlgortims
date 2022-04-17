a72689b5ca49536fae84381f539aebc64406e0d9
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-17 01:57:03 +0100

    Time: 65 ms (76.33%), Space: 41.8 MB (82.79%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.js
+++ 367-valid-perfect-square/367-valid-perfect-square.js
@@ -3,10 +3,13 @@
  * @return {boolean}
  */
 var isPerfectSquare = function(num) {
-   for (let i=0 ; i < 2147483648; i++){
-      let value =  Math.pow(i,2)
-      if(value == num) return true
-      if(value > num) return false
-   }
+    let l =0 , r=num
+    while(l <= r){
+        let pivot = Math.floor((l+r)/2)
+        let value = Math.pow(pivot, 2)
+        if(value == num ) return true
+        if(value > num )  r = pivot - 1
+        if(value < num )  l = pivot + 1
+    }
     return false
 };
