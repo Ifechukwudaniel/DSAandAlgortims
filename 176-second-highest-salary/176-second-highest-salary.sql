b618f536949e5b4277da11d44d1d9d29826d9b4e
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-18 11:29:01 +0100

    Time: 206 ms (60.69%), Space: 0B (100.00%) - LeetHub

--- 176-second-highest-salary/176-second-highest-salary.sql
+++ 176-second-highest-salary/176-second-highest-salary.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
 Select 
-    IFNULL((Select DISTINCT  salary   from  Employee order by  salary DESC Limit 1,1 ), Null)
+    (Select DISTINCT  salary   from  Employee order by  salary DESC Limit 1,1 )
     as  SecondHighestSalary
