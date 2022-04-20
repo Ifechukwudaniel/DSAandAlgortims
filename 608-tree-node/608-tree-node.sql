e4652401ba6b868f27cf3814be8e5a577b4fa536
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-20 11:09:49 +0100

    Time: 354 ms (92.43%), Space: 0B (100.00%) - LeetHub

--- 608-tree-node/608-tree-node.sql
+++ 608-tree-node/608-tree-node.sql
@@ -3,8 +3,8 @@ Select id, 'Root' as type from Tree
     where p_id IS NUll
 Union 
 Select id, 'Leaf' as type   from Tree
-    where id  not in 
-    (Select Distinct p_id from Tree where  p_id is Not Null) and  p_id is Not Null
+    where  p_id is Not Null and  id  not in 
+    (Select Distinct p_id from Tree where  p_id is Not Null) 
 Union
 Select  id, 'Inner' as type  from Tree
     where p_id is Not Null and   id  in
