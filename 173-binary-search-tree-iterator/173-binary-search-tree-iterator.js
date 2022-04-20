7105b593ac7978c265e0ae70278c42bb74aefb2d
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-20 10:25:13 +0100

    Time: 3846 ms (5.11%), Space: 63.3 MB (5.53%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var BSTIterator = function(root) {
+    this._root= root
+    this._current = 0
+    this._nodeTotal = this._nodeCount(root)
+};
+
+BSTIterator.prototype._nodeCount = function(node,count=0){
+    if(!node) return 0
+    return 1 + this._nodeCount(node.left) +  this._nodeCount(node.right)
+}
+
+/**
+ * @return {number}
+ */
+BSTIterator.prototype._inOrder = function(node,array){
+   if(!node) return null
+   this._inOrder(node.left,array)
+   array.push(node.val)
+   this._inOrder(node.right,array)
+}
+
+BSTIterator.prototype.next = function() {
+  let array =[]
+  this._inOrder(this._root,array)
+  ++this._current
+  return array[this._current -1]
+};
+
+/**
+ * @return {boolean}
+ */
+BSTIterator.prototype.hasNext = function() {
+   return this._current < this._nodeTotal
+};
+
+/** 
+ * Your BSTIterator object will be instantiated and called as such:
+ * var obj = new BSTIterator(root)
+ * var param_1 = obj.next()
+ * var param_2 = obj.hasNext()
+ */
