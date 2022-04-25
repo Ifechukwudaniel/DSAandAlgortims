7dc2415a8814c638b88ec87257c223c6a1e391d6
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-25 15:53:23 +0100

    Time: 1043 ms (5.12%), Space: 50.3 MB (5.12%) - LeetHub

---
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+     for(let i =0 ; i <arr.length; i++){
+        for(let j =0 ; j <arr.length; j++){          
+             console.log(arr[i] , 2*arr[j])
+             if(arr[i] == 2*arr[j] && i != j) return true 
+         }
+     }
+    return false
+};
+
+// [10,2,5,3]
+// [2,3,5,6,10]
+// let l =0 , r = 3
+// mid = 2*3 = 6
+
+// let l =1 , r = 3
+// mid =2 =2*5 = 10 == r
