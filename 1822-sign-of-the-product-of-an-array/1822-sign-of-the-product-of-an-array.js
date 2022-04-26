5c7e29db25042e5101c82e4d1e8b500ed03e31f8
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-26 13:43:50 +0100

    Time: 106 ms (14.05%), Space: 46.9 MB (5.88%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.js
@@ -3,9 +3,11 @@
  * @return {number}
  */
 var arraySign = function(nums) {
+    
     let  product = 1n
     
     for(let n of nums){
+        if(n == 0) return 0
          product*=BigInt(n)
     }
     
