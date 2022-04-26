d413ccfc63ebf451bb115be207aa9b3aa75928a2
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-04-26 15:26:51 +0100

    Time: 74 ms (57.99%), Space: 43.7 MB (11.54%) - LeetHub

---
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.js
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumNumbers = function(root) {
+    if(!root.left && !root.right) return root.val
+    let numberArray =  []
+    inOrder(root, numberArray)
+    let sum = 0
+    for (let num of numberArray){
+        sum= sum + (+num)
+    }
+    console.log(numberArray)
+    return  sum
+};
+
+var inOrder =  function(node, array,sofar=""){
+    if (!node) return null 
+    
+    sofar+=node.val
+    if(!node.left && !node.right) array.push(sofar)
+    inOrder(node.left ,array,sofar) 
+    inOrder(node.right, array,sofar,)
+}
+
