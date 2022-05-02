dcaec0e7bd444c2460ef92836ef7a065a67f7bd8
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-02 22:54:38 +0100

    Time: 134 ms (17.33%), Space: 44.8 MB (43.24%) - LeetHub

--- 905-sort-array-by-parity/905-sort-array-by-parity.js
+++ 905-sort-array-by-parity/905-sort-array-by-parity.js
@@ -3,18 +3,6 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    let answer = []
-    
-    for( let num of nums){
-        if(num%2 ==0){
-            answer.push(num)
-        }
-    }
-    
-   for( let num of nums){
-        if(num%2 ==1){
-            answer.push(num)
-        }
-    }
-    return answer
+    nums = nums.sort((a, b) => a%2 - b%2 || a-b)
+    return nums
 };
