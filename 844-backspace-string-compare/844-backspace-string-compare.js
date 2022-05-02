f88daf2e226c6036b013d56a847e500400a2c3c3
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-02 22:47:48 +0100

    Time: 95 ms (33.60%), Space: 42.8 MB (45.27%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.js
+++ 844-backspace-string-compare/844-backspace-string-compare.js
@@ -4,18 +4,34 @@
  * @return {boolean}
  */
 
-var build =(string)=>{
-    let  stack  = [];
-    for(let char of string){
-        if(char != '#'){
-            stack.push(char)
-        }
-        else {
-            stack.pop()
-        } 
-    }
-    return stack.join("")
-}
+
 var backspaceCompare = function(s, t) {
-   return build(s) === build(t)
+   let i = s.length -1, j = t.length-1
+   let skipS = 0, skipT = 0 
+   while(i >= 0 || j >= 0){
+       while(i>=0){
+          if(s.charAt(i) == "#") {skipS++; i--} 
+          else if (skipS > 0){
+             skipS--; i-- 
+          }
+           else break
+       }
+       while(j>=0){
+          if(t.charAt(j) == "#") {skipT++; j--} 
+          else if (skipT > 0){
+             skipT--; j-- 
+          }
+           else break
+       }
+       
+       if(i>=0 && j>=0 && s.charAt(i) !== t.charAt(j)){
+           return false
+       }
+       
+       if((i>=0) !== (j>=0)) {
+            return false 
+       }
+       i--; j--;
+   }
+    return true
 };
