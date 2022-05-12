ab1fe6f2cf29d41b3c0062371b0cc4d543c8f1f6
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-12 10:15:09 +0100

    Time: 136 ms (13.74%), Space: 44.2 MB (28.19%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.js
+++ 7-reverse-integer/7-reverse-integer.js
@@ -8,10 +8,8 @@ var reverse = function(num) {
     let max = Math.pow(2,31 ) -1
     let rev = 0;
     while(num !== 0){
-        // console.log((max/10),(min/10), rev, num)
         if(rev>(max/10) || rev < (min/10)) return 0 
         let pop = num%10
-        console.log(pop, num)
         rev = rev*10 + pop
         num = Math.trunc(num/10)
     }
