a74a99338c39dd2a1b04c4fc4891007cca8bf32e
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-16 13:02:09 +0100

    Time: 140 ms (5.18%), Space: 44.4 MB (5.88%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    if(!root) return []
+    let queue= [root]
+    let result = []
+    while(queue.length){
+        let n = queue.length
+        for(let i =0; i<n; i++){
+            let node = queue.shift()
+            console.log(node.val,i,n)
+            if(i == n- 1) result.push(node.val)
+            if(node.left) queue.push(node.left)
+            if(node.right) queue.push(node.right)
+        }
+    }
+    return result
+};
