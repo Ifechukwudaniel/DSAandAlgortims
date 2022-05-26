687c2c1c4d5fbd64901dc2380685d5bb6368a12e
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-26 12:02:26 +0100

    Time: 79 ms (63.92%), Space: 42.3 MB (76.62%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var hammingWeight = function(n) {
-    let count =0 ;
-    while( n >0){
-        // console.log(n>>1, n&1)
-        if(n&1 == 1) ++count
-        n= n >>> 1
+    let count =0;
+    while(n>0){
+        if(n&1==1)count++
+        n = n>>>1
     }
     return count
 };
