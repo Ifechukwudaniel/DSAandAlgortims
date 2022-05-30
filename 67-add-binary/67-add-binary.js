52b37735c21af14864d8c7bf23705b576abb3c1c
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-05-30 14:58:06 +0100

    Time: 221 ms (5.08%), Space: 47.9 MB (6.24%) - LeetHub

---
+++ 67-add-binary/67-add-binary.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+  let sum =0
+  let carry = 0
+  let result =""
+   
+  while(a||b||carry){
+      sum = +a.slice(-1) + +b.slice(-1) + carry
+      
+      if(sum >1){
+          result =  (sum%2) + result
+          carry = 1
+      }
+      else {
+          result = sum+ result
+          carry = 0
+      }
+      
+      a=a.slice(0,-1)
+      b= b.slice(0,-1)
+      console.log(result)
+  }
+    
+  return result 
+};
