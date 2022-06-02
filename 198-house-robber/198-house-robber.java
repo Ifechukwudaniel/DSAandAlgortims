33d65a20afcfa07416bc3727ba7a8d586fc745ba
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-02 10:02:55 +0100

    Time: 0 ms (100.00%), Space: 41.4 MB (49.82%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,23 +1,18 @@
-import java.util.Arrays;
 
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length ==1) return nums[0];
+       if(nums.length ==1) return nums[0];
        if(nums.length <=2) {
            return Math.max(nums[0], nums[1]);
        }
-        int[] table = new int[nums.length];
-        Arrays.fill(table,0);
-        table[0] = nums[0];
-        table[1] = nums[1];
-        
-        for(int i=2; i<table.length; i++){
-            int ta=0;
-            int tb=0;
-            if((i-2) >=0) ta = nums[i]+table[i-2];    
-            if((i-3) >=0) tb = nums[i]+table[i-3];
-            table[i] = Math.max(ta,tb);
+        int rob1=0;
+        int rob2 =0;
+        for(int n : nums){
+            int temp = Math.max(n+rob1, rob2);
+            rob1= rob2;
+            rob2= temp;
         }
-        return Math.max(table[table.length-2], table[table.length-1]);
+        return rob2;
+
     }
 }
