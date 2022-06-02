02ddeffdfa234a3c24cb62c38d17ab3cc55f29a0
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-02 08:58:21 +0100

    Time: 65 ms (91.56%), Space: 42.4 MB (85.49%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -2,17 +2,15 @@
  * @param {number[]} cost
  * @return {number}
  */
-
-
 var minCostClimbingStairs = function(cost) {
-   let n =cost.length
-   let d1= cost[0]
-   let d2=  cost[1]
-   
-   for(let i =2 ; i<cost.length; i++){
-      let current = cost[i]+Math.min(d1, d2)  
-      d1=d2
-      d2=current
-   }
+    let n = cost.length 
+    let d1= cost[0]
+    let d2= cost[1]
+    
+    for(let i =2; i<n; i++){
+        let current = cost[i]+Math.min(d1, d2)
+        d1 = d2
+        d2= current
+    }
     return Math.min(d1,d2)
 };
