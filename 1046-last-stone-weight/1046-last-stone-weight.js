a585223c41661d3cfddbad8245d23480d216cac8
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-03 11:17:25 +0100

    Time: 131 ms (8.25%), Space: 42.5 MB (80.90%) - LeetHub

---
+++ 1046-last-stone-weight/1046-last-stone-weight.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function(stones) {
+    let heap =  new MaxPriorityQueue();
+    for(let stone of stones){
+         heap.enqueue(stone);
+    }
+    while(heap.size()>=2){
+        let y =  heap.dequeue().element
+        let x =  heap.dequeue().element
+        // console.log(x,y)
+        if(x!==y) {
+          y = y-x
+          heap.enqueue(y)
+        }
+     }
+    
+    if(heap.size() < 1) return 0
+    return heap.dequeue().element
+};
