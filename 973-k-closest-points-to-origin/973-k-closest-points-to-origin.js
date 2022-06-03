09bce0fd715ba53b0fdea536fdeaaf6c6292b2c8
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-03 12:35:30 +0100

    Time: 339 ms (37.89%), Space: 64.9 MB (17.01%) - LeetHub

--- 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -4,19 +4,23 @@
  * @return {number[][]}
  */
 var kClosest = function(points, k) {
-    let heep = new MinPriorityQueue()
-     let origin = [0,0]
-     let a, b , sqrt
-     let  res = []
-    for(let point of points){
-       a = Math.abs(Math.pow((origin[0] -point[0]),2)) 
-       b =  Math.abs(Math.pow((origin[1] -point[1]),2))
-       sqrt = Math.abs(Math.sqrt(a+b))
-       heep.enqueue(point,sqrt)
+    let maxPQ = new MaxPriorityQueue()
+    for (let point of points) {
+        let dist = squaredDistance(point)
+        if (maxPQ.size() < k) {
+            // Fill the max PQ up to k points
+            maxPQ.enqueue(point, dist)
+        } else if (dist < maxPQ.front().priority) {
+            // If the max PQ is full and a closer point is found,
+            // discard the farthest point and add this one
+            maxPQ.dequeue()
+            maxPQ.enqueue(point, dist)
+        }
     }
-    while(k){
-        res.push(heep.dequeue().element)
-        k--
-    }
-    return res
+    
+    // Return all points stored in the max PQ
+    return maxPQ.toArray().map(el => el.element)
 };
+
+// Calculate and return the squared Euclidean distance
+const squaredDistance = ([x,y]) => x ** 2 + y ** 2
