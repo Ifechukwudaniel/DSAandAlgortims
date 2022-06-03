aebc024072ff3acb17d832ddbc28a5bf489175a0
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-03 09:35:35 +0100

    Time: 82 ms (56.69%), Space: 44.6 MB (93.23%) - LeetHub

--- 704-binary-search/704-binary-search.js
+++ 704-binary-search/704-binary-search.js
@@ -5,13 +5,18 @@
  */
 var search = function(nums, target) {
     let left = 0
-    let right = nums.length-1
+    let right = nums.length -1
     let mid 
+    
     while(left<= right){
-      mid =  Math.floor((left+right)/2)  
-      if(nums[mid] < target) left = mid+1
-      if(nums[mid] > target) right = mid- 1
-      if(nums[mid] == target) return mid   
-     }
+        mid = Math.floor((left+right)/2)
+        if(nums[mid] === target) return mid
+        if(nums[mid] < target){
+            left = mid +1
+        }
+         if(nums[mid] > target){
+            right = mid -1
+        }
+    }
     return -1
 };
