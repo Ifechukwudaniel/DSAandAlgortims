ce94cdb5ce91036ad4418273eaf85b28dd000c27
Ifechuku  Daniel <mailidanielifechukwu@gmail.com>
2022-06-07 22:05:41 +0100

    Time: 83 ms (44.81%), Space: 42 MB (77.07%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    nums1.splice(m, m+n,...nums2);
+    nums1.sort((a,b) => a-b);
+};
