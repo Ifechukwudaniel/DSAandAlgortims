8da268e9e61894bd2885ebd6f1451367f9312d4b
0xDaniel <mailidanielifechukwu@gmail.com>
2022-08-03 14:42:25 +0100

    Time: 103 ms (27.78%), Space: 41.7 MB (94.20%) - LeetHub

--- 75-sort-colors/75-sort-colors.js
+++ 75-sort-colors/75-sort-colors.js
@@ -4,23 +4,27 @@
  */
 var sortColors = function(nums) {
     let count = [0,0,0]
+    let countPos = 0
     
-    for ( let i =0; i <nums.length; i++){
-        count[nums[i]]++
-     }
-    
-    count[1]+= count[0]
-    count[2] += count[1]
-    
-    for(let i = 0; i<count[0]; i++){
-        nums[i] = 0
+    for(let i =0; i<nums.length; i++){
+        let index = nums[i]
+        count[index] +=1
     }
     
-    for(let i = count[0]; i<count[1]; i++){
+    for(let i =1; i<count.length; i++){
+        count[i]+= count[i-1] 
+    }
+    
+    for(let i =0; i<count[0]; i++){
+        nums[i] = 0;
+    }
+    
+    for(let i =count[0]; i<count[1]; i++){
         nums[i] = 1
     }
     
-    for(let i = count[1]; i<count[2]; i++){
+    for(let i =count[1]; i<count[2]; i++){
         nums[i] = 2
     }
+    
 };
