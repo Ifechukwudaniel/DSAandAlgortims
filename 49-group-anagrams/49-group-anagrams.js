3a0b5210fd5686bdd891b45acaf1d2c5ac3fed21
0xDaniel <mailidanielifechukwu@gmail.com>
2022-08-03 15:50:37 +0100

    Time: 118 ms (97.48%), Space: 51.5 MB (99.23%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.js
+++ 49-group-anagrams/49-group-anagrams.js
@@ -3,16 +3,17 @@
  * @return {string[][]}
  */
 var groupAnagrams = function(strs) {
-     let map = new Map();
-
-  for (let word of strs ){
-    let key = word.split("").sort().join("#")
-    if(!map.has(key)){
-      map.set(key, [word])
-    }else {
-        let data = map.get(key)
-        data.push(word)
+    let map = new Map();
+    
+    for(let word of strs){
+      let key = word.split("").sort().join("#")
+      if(!map.has(key)){
+         map.set(key,[word])
+      }
+      else{
+           let data = map.get(key)  
+           data.push(word)
+      }
     }
-  }
-  return [...map.values()]
+    return [...map.values()]
 };
