d29ddee484845ae2dcce6075a41d5a87d2633591
0xDaniel <mailidanielifechukwu@gmail.com>
2022-08-03 15:00:24 +0100

    Time: 99 ms (80.42%), Space: 44.4 MB (47.47%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -3,13 +3,15 @@
  * @param {string} t
  * @return {boolean}
  */
-let hash=(string)=>{
+
+var hash = function(string) {
     let array = new Array(26).fill(0)
-    for(letter of string){
-       array[letter.charCodeAt(0) -97] += 1
+    for(let char of string){
+        array[char.charCodeAt()-97] +=1
     }
     return array.join("#")
-}
+};
+
 var isAnagram = function(s, t) {
-    return hash(s) === hash(t)
+   return  hash(s) === hash(t)
 };
