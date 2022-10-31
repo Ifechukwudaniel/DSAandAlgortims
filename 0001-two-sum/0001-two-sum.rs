6ee9b2c2520e472929b65c32febfa76de5968ca1
0xDaniel <mailidanielifechukwu@gmail.com>
2022-10-31 15:32:01 +0100

    Time: 35 ms (26.22%), Space: 2.2 MB (67.48%) - LeetHub

--- 0001-two-sum/0001-two-sum.rs
+++ 0001-two-sum/0001-two-sum.rs
@@ -1,14 +1,5 @@
 impl Solution {
-         pub fn two_sum(nums: Vec<i32>, target: i32) ->Vec<i32> {
-        if (nums.len() < 2 || nums.len() > 10_0000) || (target < -1000_000_000 || target > 1000_000_000){
-            panic!("Wrong input data");
-        }
-        for &val in nums.iter() {
-            if val < -100_0000_000 || val > 1000_000_000{
-                panic!("Too large or too small value in vec");
-            }
-        }
-            
+         pub fn two_sum(nums: Vec<i32>, target: i32) ->Vec<i32> { 
         for (i, val_i) in nums.iter().enumerate() {
             for j in i+1..nums.len() {
                 if val_i + nums[j] == target {
