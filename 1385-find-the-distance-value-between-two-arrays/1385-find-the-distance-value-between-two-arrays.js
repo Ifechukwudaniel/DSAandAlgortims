51193603ab337e82341fb59c7db47f1fed2e9624
0xDaniel <mailidanielifechukwu@gmail.com>
2022-11-18 08:56:39 +0100

    Time: 125 ms (26.05%), Space: 44.7 MB (17.67%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.js
@@ -4,14 +4,38 @@
  * @param {number} d
  * @return {number}
  */
-var findTheDistanceValue = function(arr1, arr2, d) { 
-    let count = 0
-    for(let i=0; i<arr1.length; i++){
-        let passes = true
-        for(let j=0; j<arr2.length; j++){
-         if(Math.abs(arr1[i] - arr2[j]) <= d)  passes= false
+var findTheDistanceValue = function(arr1, arr2, d) {
+    const arr2Sorted = arr2.sort((a, b) => a - b);
+    let dist = 0;
+    
+    for (const num of arr1) {
+        if (isDistanceValid(num, d, arr2Sorted)) {
+            dist += 1;
         }
-      if(passes) count++
     }
-    return count
+    
+    return dist;
 };
+
+function isDistanceValid(number, dist, array) {
+    let left = 0;
+    let right = array.length - 1;
+    
+    while (left <= right) {
+        const mid = Math.floor((right + left) / 2);
+        
+        if (Math.abs(number - array[mid]) <= dist) {
+            return false;
+        }
+        
+        if (array[mid] < number) {
+            left = mid + 1;
+        }
+        
+        if (array[mid] > number) {
+            right = mid - 1;
+        }
+    }
+    
+    return true;
+}
