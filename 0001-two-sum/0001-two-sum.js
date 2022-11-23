618d19fb5be78e18608bb0829f45dac0d5ff7631
0xDaniel <mailidanielifechukwu@gmail.com>
2022-11-23 16:58:03 +0100

    Time: 112 ms (67.61%), Space: 43.1 MB (32.43%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -8,9 +8,7 @@ var twoSum = function(nums, target) {
     
     for (let i =0;i<nums.length; i++){
         let currentTarget =  target - nums[i]
-        if(map.has(currentTarget)) {
-          return [map.get(currentTarget),i]
-        }
+        if(map.has(currentTarget))  return [map.get(currentTarget),i]
         map.set(nums[i],i)
     }
     return []
